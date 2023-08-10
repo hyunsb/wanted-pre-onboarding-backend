85c39a76027a228648edc8821b65807067e93da8
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-11 00:49:14 +0900

    chore: 프로젝트 개발 환경 설정 (#3)

---
+++ src/main/java/com/hyunsb/wanted/WantedApplication.java
@@ -0,0 +1,11 @@
+package com.hyunsb.wanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WantedApplication.class, args);
+	}
+}
