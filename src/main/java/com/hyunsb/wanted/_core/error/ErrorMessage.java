c6f1a95d634a03a31d61b6840e488841be4ad68d
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-11 22:24:45 +0900

    chore: 에레메시지 클래스 디렉터리 변경 (#9)

---
+++ src/main/java/com/hyunsb/wanted/_core/error/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.hyunsb.wanted._core.error;
+
+public class ErrorMessage {
+
+    public static final String INVALID_EMAIL_FORMAT = "유효하지 않은 이메일 형식입니다.";
+    public static final String INVALID_PASSWORD_LENGTH = "비밀번호는 8자 이상이어야 합니다.";
+    public static final String USER_NOT_FOUND = "아이디와 비밀번호를 다시 확인해주세요.";
+
+    public static final String TOKEN_UN_AUTHORIZED = "토큰 인증에 실패하였습니다.";
+    public static final String TOKEN_EXPIRED = "토큰이 만료되었습니다.";
+    public static final String TOKEN_VERIFICATION_FAILED = "올바른 토큰이 아닙니다.";
+
+    public static final String INVALID_USER = "유효하지 않은 회원 정보입니다.";
+}
