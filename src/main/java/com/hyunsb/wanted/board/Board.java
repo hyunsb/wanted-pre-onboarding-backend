5e8deb190d90d15830e668d7314f433a0c6cc14b
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-11 22:23:30 +0900

    feat: JPA 엔티티로 게시글 테이블 모델링 (#9)

---
+++ src/main/java/com/hyunsb/wanted/board/Board.java
@@ -0,0 +1,31 @@
+package com.hyunsb.wanted.board;
+
+import com.hyunsb.wanted.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "board_tb")
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false)
+    @Lob
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
