8b91e739c932f55f9550107b8dde333fe617f728
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-12 01:27:00 +0900

    feat: 게시글 목록 조회 엔드포린트 구현 (#11)

--- src/main/java/com/hyunsb/wanted/board/BoardController.java
+++ src/main/java/com/hyunsb/wanted/board/BoardController.java
@@ -1,10 +1,15 @@
 package com.hyunsb.wanted.board;
 
+import com.hyunsb.wanted._core.error.exception.UnauthorizedException;
 import com.hyunsb.wanted._core.security.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +30,19 @@ public class BoardController {
         log.info("POST /board : " + saveDTO);
 
         Long userId = (Long) request.getAttribute(JwtProvider.REQUEST);
+        if (userId == null) throw new UnauthorizedException();
+
         boardService.save(saveDTO, userId);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/board")
+    public ResponseEntity<Object> getAllList(
+            @PageableDefault(size = 5) Pageable pageable) {
+        log.info("GET /board : " + pageable);
+
+        Page<BoardResponse.ListDTO> allList = boardService.getAllList(pageable);
+        return ResponseEntity.ok(allList);
     }
 }
