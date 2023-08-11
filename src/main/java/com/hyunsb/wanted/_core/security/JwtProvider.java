3ca297c000c5c6cbaa100cdd8ff4e8153f022d95
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-11 12:18:43 +0900

    feat: JWT 관련 클래스 구현 및 환경변수에 비밀 키 저장 (#7)

---
+++ src/main/java/com/hyunsb/wanted/_core/security/JwtProvider.java
@@ -0,0 +1,53 @@
+package com.hyunsb.wanted._core.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.hyunsb.wanted.user.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Date;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JwtProvider {
+
+    public static final Long EXP = 1000L * 60 * 60;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER = "Authorization";
+
+    private final Environment environment;
+    private String key;
+
+    @PostConstruct
+    private void init() {
+        key = environment.getProperty("JWT_SECRET_KEY");
+    }
+
+    public String create(User user) {
+        String jwt = JWT.create()
+                .withSubject(user.getEmail())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXP))
+                .withClaim("email", user.getEmail())
+                .withClaim("id", user.getId())
+                .sign(Algorithm.HMAC512(key));
+
+        log.info("JWT created: authentication object is creation");
+        return TOKEN_PREFIX + jwt;
+    }
+
+    public DecodedJWT verify(String jwt) throws SignatureVerificationException, TokenExpiredException {
+        log.info("JWT verify: " + jwt);
+        String origin = jwt.replace(JwtProvider.TOKEN_PREFIX, "");
+        return JWT.require(Algorithm.HMAC512(key))
+                .build()
+                .verify(origin);
+    }
+}
