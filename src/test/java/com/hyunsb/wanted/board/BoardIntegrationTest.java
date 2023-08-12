452efb98313c48b5cb9c747aeef9348209baeeb6
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-12 17:42:18 +0900

    test: 게시글 목록 조회 컨트롤러, 서비스 단위테스트 및 통합테스트 작성 (#11)

--- src/test/java/com/hyunsb/wanted/board/BoardIntegrationTest.java
+++ src/test/java/com/hyunsb/wanted/board/BoardIntegrationTest.java
@@ -1,11 +1,18 @@
 package com.hyunsb.wanted.board;
 
 import com.hyunsb.wanted._core.error.exception.BoardSaveFailureException;
+import com.hyunsb.wanted._core.error.exception.ExceededMaximumPageSizeException;
 import com.hyunsb.wanted.user.User;
 import com.hyunsb.wanted.user.UserRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @SpringBootTest
 public class BoardIntegrationTest {
@@ -15,6 +22,9 @@ public class BoardIntegrationTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BoardRepository boardRepository;
 
     @BeforeEach
     void setUp() {
@@ -62,6 +72,50 @@ public class BoardIntegrationTest {
             // Then
             Assertions.assertThrows(BoardSaveFailureException.class,
                     () -> boardService.save(saveDTO, userId));
+        }
+    }
+
+    @Nested
+    @DisplayName("게시글 목록 조회 통합 테스트")
+    class GetAllList {
+
+        @DisplayName("성공")
+        @Test
+        void success_Test() {
+            // Given
+            List<Board> mockList = List.of(
+                    Board.builder().title("title").content("content").user(User.builder().id(1L).build()).build(),
+                    Board.builder().title("title").content("content").user(User.builder().id(1L).build()).build(),
+                    Board.builder().title("title").content("content").user(User.builder().id(1L).build()).build(),
+                    Board.builder().title("title").content("content").user(User.builder().id(1L).build()).build(),
+                    Board.builder().title("title").content("content").user(User.builder().id(1L).build()).build()
+            );
+
+            boardRepository.saveAll(mockList);
+
+            Pageable pageable = PageRequest.of(0, 3);
+
+            // When
+            Page<BoardResponse.ListDTO> actual = boardService.getAllList(pageable);
+
+            // Then
+            Assertions.assertAll(
+                    () -> Assertions.assertEquals(3, actual.getSize()),
+                    () -> Assertions.assertEquals(2, actual.getTotalPages()),
+                    () -> Assertions.assertEquals(5, actual.getTotalElements())
+            );
+        }
+
+        @DisplayName("실패 - 페이징 사이즈 범위 초과")
+        @Test
+        void failure_Test_ExceededPagingSize() {
+            // Given
+            Pageable pageable = PageRequest.of(0, 200);
+
+            // When
+            // Then
+            Assertions.assertThrows(ExceededMaximumPageSizeException.class, () ->
+                    boardService.getAllList(pageable));
         }
     }
 }
