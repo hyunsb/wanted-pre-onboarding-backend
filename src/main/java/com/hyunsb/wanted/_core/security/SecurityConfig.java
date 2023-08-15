e17e7a994e085de9ac537943acfa8b8691524387
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-16 03:10:41 +0900

    refactor: JwtProvider를 static 유틸 클래스가 아닌 싱글톤으로 변경 (#24)

--- src/main/java/com/hyunsb/wanted/_core/security/SecurityConfig.java
+++ src/main/java/com/hyunsb/wanted/_core/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.hyunsb.wanted._core.security;
 
 import com.hyunsb.wanted._core.error.ErrorMessage;
 import com.hyunsb.wanted._core.util.FilterResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -15,7 +17,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final Environment environment;
+
+    @Bean
+    public JwtProvider jwtProvider() {
+        return new JwtProvider(environment);
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -40,7 +50,7 @@ public class SecurityConfig {
 
                 .httpBasic().disable()
 
-                .apply(new SecurityFilterManager())
+                .apply(new SecurityFilterManager(jwtProvider()))
 
                 .and().exceptionHandling().authenticationEntryPoint((request, response, authException) ->
                         FilterResponse.unAuthorized(response, ErrorMessage.TOKEN_UN_AUTHORIZED))
@@ -56,12 +66,15 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @RequiredArgsConstructor
     public static class SecurityFilterManager extends AbstractHttpConfigurer<SecurityFilterManager, HttpSecurity> {
+
+        private final JwtProvider jwtProvider;
 
         @Override
         public void configure(HttpSecurity builder) throws Exception {
             AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
-            builder.addFilter(new JwtAuthenticationFilter(authenticationManager));
+            builder.addFilter(new JwtAuthenticationFilter(authenticationManager, jwtProvider));
             super.configure(builder);
         }
     }
